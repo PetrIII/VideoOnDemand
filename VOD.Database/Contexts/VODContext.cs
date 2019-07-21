b6766dc6c0f5694381df48ab3daec1d65f3f3deb
«KBykov» <petr123123@yandex.ru>
2019-07-21 15:35:54 +0300

    first

---
+++ VOD.Database/Contexts/VODContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using VOD.Common.Entities;
+
+namespace VOD.Database.Contexts
+{
+    public class VODContext : IdentityDbContext<VODUser>
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Download> Downloads { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<UserCourse> UserCourses { get; set; }
+        public DbSet<Video> Videos { get; set; }
+        public VODContext(DbContextOptions<VODContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<UserCourse>().HasKey(uc => new { uc.UserId, uc.CourseId });
+            foreach(var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ VOD.UI/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using VOD.UI.Models;
+using Microsoft.AspNetCore.Identity;
+using VOD.Common.Entities;
+using VOD.Database.Services;
+using System.Threading.Tasks;
+
+namespace VOD.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SignInManager<VODUser> _signInManager;
+        //private IDbReadService _db;
+
+        public HomeController(SignInManager<VODUser> signInMgr/*, IDbReadService db*/)
+        {
+            _signInManager = signInMgr;
+            //_db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (!_signInManager.IsSignedIn(User))
+                return RedirectToPage("/Account/Login", new { Area = "Identity" });
+            /*_db.Include<Download>();
+            _db.Include<Module, Course>();
+            var result1 = await _db.SingleAsync<Download>(d => d.Id.Equals(3));
+            var result2 = await _db.GetAsync<Download>();
+            var result3 = await _db.GetAsync<Download>(d=>d.ModuleId.Equals(1));
+            var result4 = await _db.AnyAsync<Download>(d=>d.ModuleId.Equals(1));*/
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
