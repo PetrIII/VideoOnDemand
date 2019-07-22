bc7f0ee2d37ad83fbd5e25b762f75f55de633fd3
PetrIII <petr123123@yandex.ru>
2019-07-22 15:22:01 +0300

    v2

--- VOD.UI/Startup.cs
+++ VOD.UI/Startup.cs
@@ -16,6 +16,7 @@ using VOD.Common.Entities;
 using VOD.Database.Contexts;
 using VOD.Database.Migrations;
 using VOD.Database.Services;
+using VOD.Common.DTOModels.UI;
 
 namespace VOD.UI
 {
@@ -31,11 +32,11 @@ namespace VOD.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
+            /*services.Configure<CookiePolicyOptions>(options =>
             {
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            });*/
 
             services.AddDbContext<VODContext>(options =>
                 options.UseSqlServer(
@@ -47,6 +48,38 @@ namespace VOD.UI
             services.AddScoped<IUIReadService, UIReadService>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Video, VideoDTO>();
+                cfg.CreateMap<Download, DownloadDTO>()
+                    .ForMember(dest => dest.DownloadUrl,
+                        src => src.MapFrom(s => s.Url))
+                    .ForMember(dest => dest.DownloadTitle,
+                        src => src.MapFrom(s => s.Title));
+                cfg.CreateMap<Instructor, InstructorDTO>()
+                    .ForMember(dest => dest.InstructorName,
+                        src => src.MapFrom(s => s.Name))
+                    .ForMember(dest => dest.InstructorDescription,
+                        src => src.MapFrom(s => s.Description))
+                    .ForMember(dest => dest.InstructorAvatar,
+                        src => src.MapFrom(s => s.Thumbnail));
+                cfg.CreateMap<Course, CourseDTO>()
+                    .ForMember(dest => dest.CourseId, src =>
+                        src.MapFrom(s => s.Id))
+                    .ForMember(dest => dest.CourseTitle,
+                        src => src.MapFrom(s => s.Title))
+                    .ForMember(dest => dest.CourseDescription,
+                        src => src.MapFrom(s => s.Description))
+                    .ForMember(dest => dest.MarqueeImageUrl,
+                        src => src.MapFrom(s => s.MarqueeImgaeUrl))
+                    .ForMember(dest => dest.CourseImageUrl,
+                        src => src.MapFrom(s => s.ImageUrl));
+                cfg.CreateMap<Module, ModuleDTO>()
+                    .ForMember(dest => dest.ModuleTitle,
+                        src => src.MapFrom(s => s.Title));
+            });
+            var mapper = config.CreateMapper();
+            services.AddSingleton(mapper);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,7 +99,7 @@ namespace VOD.UI
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            //app.UseCookiePolicy();
             //DbInitializer.RecreateDatabase(db);
             //DbInitializer.Initialize(db);
             app.UseAuthentication();
